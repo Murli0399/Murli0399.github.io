f1414ad51e03bde1eddd8a9dce1a5108df864ecb
Murli0399 <murlikhaire28@gmail.com>
2023-05-11 00:10:51 +0530

    11/05/2023 1st

---
+++ script/email.js
@@ -0,0 +1,20 @@
+function send() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "murlikhaire28@gmail.com",
+    // Password: "zqtlgxhorxphhtwm",
+    Password: "ED4DC8B959DAFB477564CB723C9C9B63A21A",
+    From: 'murlikhaire28@gmail.com',
+    To: 'murlikhaire28@gmail.com',
+    Subject: name,
+    Body: "Name : " + name + "<br/>" +
+      "Email : " + email + "<br/><br/>" +
+      " " + message
+  }).then(
+    msg => alert(msg)
+  );
+}
